058a13c7cf66cdab2a0954cb0c9e2d4130ceac00
Lívia Calderan <liv_calderan@hotmail.com>
2026-09-17 15:23:12 -0400

    feat: LinkInBio interface created

--- resources/views/link-in-bio.blade.php
+++ resources/views/link-in-bio.blade.php
@@ -1,17 +1,28 @@
 <x-layout.app>
-    <div>
-        <x-img src="/storage/{{ $user->photo  }}" alt="User Profile Picture"/>
-        <h2>User: {{ $user->name }} :: {{ $user->id }}</h2>
-        <h3>{{ $user->description }}</h3>
+    <x-container>
+        
+        <div class="text-center flex flex-col space-x-4 w-2/3 mt-4">
+            <x-img src="/storage/{{ $user->photo }}" alt="/Profile Picture" />
+            <div class="text-2xl font-bold justify-center m-3 text-primary tracking-wider">- {{ $user->name }} -</div>
+            <div class="text-sm italic opacity-80">{{ $user->description }}</div>
 
-        <ul>
-            @foreach ($user->links as $link)
-                <li>
 
-                    <a href="{{ $link->link }}" target="_blank">{{ $link->name }} </a>
+            <ul class="space-y-3 mt-6">
+                @foreach ($user->links as $link)
 
-                </li>
-            @endforeach
-        </ul>
-    </div>
+                    <li class="flex items-center gap-4 justify-center">
+
+                        <x-links-btn :route="$link->link" class="w-full btn-outline">
+                            {{ $link->name }}
+                        </x-links-btn>
+                    </li>
+
+                @endforeach
+            </ul>
+
+
+
+        </div>
+    </x-container>
+
 </x-layout.app>
